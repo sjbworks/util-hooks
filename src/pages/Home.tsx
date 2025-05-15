b791c8cb34d1bc49d792b4c207a9535e5e62f235
sjbworks <sacred.jizo.bosatz@gmail.com>
2025-05-15 17:52:12 +0900

    Update Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router";
 export const Home = () => {
-  return <h2>Home</h2>;
+  return (
+    <>
+      <h2>Home</h2>
+      <ul
+        style={{
+          listStyleType: "disc",
+          paddingLeft: "1.5rem",
+          textAlign: "left",
+        }}
+      >
+        <li>
+          <Link to="/nav-guard">Navigation Guard</Link>
+        </li>
+        <li>
+          <Link to="/scroll-tab">Scroll Tabs</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
